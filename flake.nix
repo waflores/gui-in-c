702e1ddf61b24148a8847f71a53dff823ca5ea4a
Will Flores <waflores956@gmail.com>
2025-06-01 16:05:39 -0400

    Updating nixpkgs, and adding gtkmain program.

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
   inputs = {
     # keep-sorted start
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
-    blueprint.url = "https://github.com/numtide/blueprint/archive/7ae8756a68c662d551e354beb537f365b80e5108.tar.gz";
-    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/b107b36e150478e05bd06d50bcc4f2218df0257f.tar.gz"; # 25.05 - 2025-03-26
+    blueprint.url = "https://github.com/numtide/blueprint/archive/49bbd5d072b577072f4a1d07d4b0621ecce768af.tar.gz";
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/5cfaab2c5c1492cbec3e5e85a4dca8601858a06a.tar.gz"; # 25.05 - 2025-04-24
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/adc195eef5da3606891cedf80c0d9ce2d3190808.tar.gz";
+    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/8d404a69efe76146368885110f29a2ca3700bee6.tar.gz";
     # keep-sorted end
   }; # End inputs
 
