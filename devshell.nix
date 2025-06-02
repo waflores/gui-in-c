4a29618bb4d952d79560906cd0a8f5c60dabcb90
Will Flores <waflores956@gmail.com>
2025-06-01 21:12:02 -0400

    XXX: We got gcc to work again with our devshell.

--- devshell.nix
+++ devshell.nix
@@ -3,63 +3,71 @@
   pkgs,
   ...
 }:
-pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } rec {
-  name = "gui-shell";
-  xorgDeps = with pkgs.xorg; [
-    libX11
-    libXrender
-    libXrandr
-    libxcb
-    libXmu
-    libpthreadstubs
-    libXext
-    libXdmcp
-    libXxf86vm
-    libXinerama
-    libSM
-    libXv
-    libXaw
-    libXi
-    libXcursor
-    libXcomposite
-    libXfixes
-    libXtst
-    libXScrnSaver
-    libICE
-    libXt
-  ];
-  packages =
-    with pkgs;
-    [
-      # keep-sorted start block=yes case=no
-      (python3.withPackages (
-        python-pkgs: with python-pkgs; [
-          venvShellHook
-        ]
-      ))
-      bashInteractive
-      bazelisk
-      clang
-      clang-tools
-      compiledb
-      direnv
-      fzf
-      gzip
-      jdk8
-      libdrm
-      mesa
-      mesa-demos
-      mesa.spirv2dxil
-      ninja
-      nix-output-monitor
-      pkg-config
-      SDL2
-      SDL2.dev
-      tree
-      vim
-      # keep-sorted end
-    ]
-    ++ xorgDeps;
-}
+pkgs.mkShell.override
+  {
+    # stdenv = pkgs.clangStdenv;
+  }
+  rec {
+    name = "gui-shell";
+    xorgDeps = with pkgs.xorg; [
+      libX11
+      libXrender
+      libXrandr
+      libxcb
+      libXmu
+      libpthreadstubs
+      libXext
+      libXdmcp
+      libXxf86vm
+      libXinerama
+      libSM
+      libXv
+      libXaw
+      libXi
+      libXcursor
+      libXcomposite
+      libXfixes
+      libXtst
+      libXScrnSaver
+      libICE
+      libXt
+    ];
+    packages =
+      with pkgs;
+      [
+        # keep-sorted start block=yes case=no
+        (python3.withPackages (
+          python-pkgs: with python-pkgs; [
+            venvShellHook
+          ]
+        ))
+        bashInteractive
+        bazel-buildtools
+        cacert
+        nix
+        git
+        bazelisk
+        clang
+        clang-tools
+        compiledb
+        direnv
+        fzf
+        gzip
+        jdk8
+        libdrm
+        mesa
+        mesa-demos
+        mesa.spirv2dxil
+        ninja
+        nix-output-monitor
+        pkg-config
+        SDL2
+        SDL2.dev
+        tree
+        vim
+        # keep-sorted end
+      ]
+      ++ xorgDeps;
+  }
 
 # https://github.com/numtide/blueprint/blob/main/docs/content/guides/configuring_direnv.md
