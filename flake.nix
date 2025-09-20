bc18d230dabc4e41f995862ba114c7cd080299d0
Will Flores <waflores956@gmail.com>
2025-09-19 21:18:48 -0400

    Updating versions of toolchains.

--- flake.nix
+++ flake.nix
@@ -7,9 +7,9 @@
     # keep-sorted start
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
     blueprint.url = "https://github.com/numtide/blueprint/archive/3c8bf84e28df2be19cc6623cb3ceeb6fc0839b91.tar.gz";
-    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/934c891d823f663fdb259ce6060a4fa410f76d65.tar.gz"; # 25.05 - 2025-08-29
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/a0959a7457327eb5a98c222d5962ff95c6a3fe3e.tar.gz"; # 25.11 - 2025-09-19
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/74e1a52d5bd9430312f8d1b8b0354c92c17453e5.tar.gz";
+    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/128222dc911b8e2e18939537bed1762b7f3a04aa.tar.gz";
     # keep-sorted end
   }; # End inputs
 
