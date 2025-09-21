9d2aa4da80f24da8625889ea72263b34423587b9
Will Flores <waflores956@gmail.com>
2025-09-21 13:54:22 -0400

    Testing a struct cast experiment.

--- src/tutorials/helloworld/expre_CastOperator.cpp
+++ src/tutorials/helloworld/expre_CastOperator.cpp
@@ -1,5 +1,7 @@
 // expre_CastOperator2.cpp
 // The following sample shows how to define and use a cast operator.
+#include <cstdint>
+#include <iostream>
 #include <stdio.h>
 #include <string.h>
 
@@ -31,6 +33,18 @@ private:
   const size_t m_nSize;
 };
 
+struct message_header {
+  uint32_t type;
+  uint32_t source;
+  uint32_t destination;
+};
+
+std::ostream &operator<<(std::ostream &s, const message_header &mh) {
+  return s << "type: " << std::to_string(mh.type)
+           << ", source: " << std::to_string(mh.source)
+           << ", destination: " << std::to_string(mh.destination);
+}
+
 int main() {
   const char *kStr = "Excitinggg";
   CountedAnsiString myStr(kStr, 8);
@@ -43,4 +57,12 @@ int main() {
 
   puts("Note that the cast changed the raw internal string");
   printf("Raw Bytes after cast:   %s\n", pRaw);
+
+  message_header m{0x50, 32, 33};
+  std::cout << "size of message header: " << sizeof(m) << std::endl;
+  std::cout << "message header: " << m << std::endl;
+
+  uint32_t bare_header[]{0x50, 33, 32};
+  auto converted_header = reinterpret_cast<message_header &>(bare_header);
+  std::cout << "our new message header: " << converted_header << std::endl;
 }
